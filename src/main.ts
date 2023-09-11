b0d19e4fe974955b246fc7d02110fdfa3af43989
Decxo360 <diegolundstedt99@gmail.com>
2023-09-11 19:44:16 -0300

    feat: configuration for class validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Prefijo antes de cualquier ruta de un endpoint, url base
+  app.setGlobalPrefix("api/v1")
+
+  //configuracion para validar datos de los endpoints, en caso de ser string y 
+  //necesita un number lo transforma de manera automática
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:true,
+      forbidNonWhitelisted:true,
+      transform: true
+    })
+  )
+
   await app.listen(3000);
 }
 bootstrap();
