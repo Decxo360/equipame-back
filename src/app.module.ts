0f75bc633b560ec71ac7327f36489e440c789251
Decxo360 <diegolundstedt99@gmail.com>
2023-09-11 19:42:07 -0300

    feat: connection to db using TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host:'localhost',
+      port:3307,
+      username:"admin",
+      password:"root",
+      database:"db_crud",
+      autoLoadEntities: true,
+      synchronize:true
+    }),
+    UsersModule
+  ],
   controllers: [],
   providers: [],
 })
